d0f0b30652dab77c5e8a1afc2bbe414ab50d5a55
dski97 <dcwalinski97@gmail.com>
2023-08-27 21:37:24 +0000

    update https

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const app = express();
 const path = require('path');
 const petconnect = require('./petconnect');
 
+// Read the SSL certificate files
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+
+// Create credentials object
+const credentials = { key: privateKey, cert: certificate };
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Start the server
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server running on http://0.0.0.0:3000');
+// Create HTTPS server
+const httpsServer = https.createServer(credentials, app);
+
+// Start the HTTPS server
+httpsServer.listen(3000, '0.0.0.0', () => {
+  console.log('HTTPS Server running on https://0.0.0.0:3000');
 });
 
 let accessToken;
 
 // Authenticate the Petfinder API
 petconnect.getAccessToken().then(token => {
-  // Store the access token for subsequent requests
   accessToken = token;
 
-  // Add a new endpoint to fetch organizations
   app.get('/organizations', (req, res) => {
     petconnect.getOrganizations(accessToken)
       .then(organizations => {
-        res.json(organizations.organizations); // Send organizations to the client
+        res.json(organizations.organizations);
       })
       .catch(error => {
         console.error('Error fetching organizations:', error);
@@ -36,7 +46,7 @@ app.get('/animals', (req, res) => {
   const organizationId = req.query.organization;
   petconnect.getAnimalsByOrganization(accessToken, organizationId)
     .then(animals => {
-      res.json(animals.animals); // Send animals to the client
+      res.json(animals.animals);
     })
     .catch(error => {
       console.error('Error fetching animals:', error);
